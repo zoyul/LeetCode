be34bef283314acc2f39901769466f3b903f29e7
zoyul <91842782+zoyul@users.noreply.github.com>
2023-03-27 17:20:04 +0900

    Time: 75 ms (46.43%), Space: 14.6 MB (78.88%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -4,15 +4,13 @@ class Solution(object):
         n = len(grid)
         m = len(grid[0])
 
-        sum_grid = [[0] * m for _ in range(n)]
-
-        for i in range(m):
-            sum_grid[0][i] = sum_grid[0][i-1] + grid[0][i]
+        for i in range(1, m):
+            grid[0][i] += grid[0][i-1]
         for i in range(1, n):
-            sum_grid[i][0] = sum_grid[i-1][0] + grid[i][0]
+            grid[i][0] += grid[i-1][0]
 
         for i in range(1, n):
             for j in range(1, m):
-                sum_grid[i][j] = min(sum_grid[i][j-1], sum_grid[i-1][j]) + grid[i][j]
+                grid[i][j] += min(grid[i][j-1], grid[i-1][j])
 
-        return sum_grid[-1][-1]
+        return grid[-1][-1]
