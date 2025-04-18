168ed61cb63c98e88027bb093d1374d12331cf07
zoyul <91842782+zoyul@users.noreply.github.com>
2025-04-18 20:30:30 +0900

    Time: 17 ms (14.36%), Space: 45.1 MB (14.75%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,44 +1,25 @@
 class Solution {
     public String countAndSay(int n) {
         if (n == 1) return "1";
-        String rle = "1";
-        for (int i = 1; i < n ; i ++) {
-            rle = rle(rle);
-        }
-        return rle;
-    }
 
-    private String rle(String input) {
-        int l = input.length();
-        if (l == 1) return "11";
-
+        String prev = countAndSay(n-1);
         String result = "";
 
         int cnt = 1;
-        char before = input.charAt(0);
+        int l = prev.length();
         for (int i = 1; i < l; i++) {
-            char now = input.charAt(i);
-            // 이전과 지금이 같다면 카운트 세기
-            if (before == now) {
+            if (prev.charAt(i - 1) == prev.charAt(i)) {
                 cnt ++;
-                // 맨 마지막 글자면 따로 더해주기
-                if (i == l - 1) {
-                    result += cnt;
-                    result += before;
-                }
             } else {
-                // 이전과 지금이 같지 않다면 rle에 결과 넣어주고 카운트 초기화
                 result += cnt;
-                result += before;
+                result += prev.charAt(i - 1);
                 cnt = 1;
-                // 맨 마지막 글자면 따로 더해주기
-                if (i == l - 1) {
-                    result += 1;
-                    result += now;
-                }
             }
-            before = now;
         }
+
+        result += cnt;
+        result += prev.charAt(l - 1);
+
         return result;
     }
 }
