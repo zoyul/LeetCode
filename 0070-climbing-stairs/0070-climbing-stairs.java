40c4e7484d3beaff92c071e5672c65e661aea7de
zoyul <91842782+zoyul@users.noreply.github.com>
2025-05-16 20:19:27 +0900

    Time: 0 ms (100%), Space: 40.4 MB (50.46%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] cnt = new int[n + 1];
+        cnt[0] = 1;
+        cnt[1] = 1;
+        for (int i = 2; i < n + 1 ; i++) {
+            cnt[i] = cnt[i-1] + cnt[i-2];
+        }
+
+        return cnt[n];
+    }
+}
