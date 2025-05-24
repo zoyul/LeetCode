d69dabde82e0eb4971fe7fbe6c59d65ac198a0ca
zoyul <91842782+zoyul@users.noreply.github.com>
2025-05-24 16:32:23 +0900

    Time: 638 ms (15%), Space: 41 MB (98.7%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,15 +1,15 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return caculate(nums, target, 0, 0);
+        return calculate(nums, target, 0, 0);
     }
 
-    private int caculate(int[] arr, int target, int idx, int sum) {
+    private int calculate(int[] arr, int target, int idx, int sum) {
         if (idx == arr.length) {
             return sum == target ? 1 : 0;
         }
 
-        int sub = caculate(arr, target, idx + 1, sum - arr[idx]);
-        int add = caculate(arr, target, idx + 1, sum + arr[idx]);
+        int sub = calculate(arr, target, idx + 1, sum - arr[idx]);
+        int add = calculate(arr, target, idx + 1, sum + arr[idx]);
         return sub + add;
     }
 }
