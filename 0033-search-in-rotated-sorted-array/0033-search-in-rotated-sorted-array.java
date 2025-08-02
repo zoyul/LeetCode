214626877c7bda5586af7788f72249edfae9e47a
zoyul <91842782+zoyul@users.noreply.github.com>
2025-08-02 18:43:19 +0900

    Time: 0 ms (100%), Space: 42.4 MB (19.62%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -10,7 +10,7 @@ class Solution {
             // 왼쪽 구간이 정렬된 경우
             if (nums[left] <= nums[mid]) {
                 if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
+                    return binarySearch(nums, left, mid - 1, target);
                 } else {
                     left = mid + 1;
                 }
@@ -18,11 +18,21 @@ class Solution {
             // 오른쪽 구간이 정렬된 경우
             else {
                 if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
+                    return binarySearch(nums, mid + 1, right, target);
                 } else {
                     right = mid - 1;
                 }
             }
+        }
+        return -1;
+    }
+
+    private int binarySearch(int[] nums, int left, int right, int target) {
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < target) left = mid + 1;
+            if (nums[mid] > target) right = mid - 1;
         }
         return -1;
     }
